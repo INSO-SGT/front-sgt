79005db91b84b6e82114a1e2991386e8252319fd
SebastianGilian <mgiliana1@upao.edu.pe>
2024-11-25 02:54:09 -0500

    dias de sesion en paciente

--- src/app/components/patients/patient-register/patient-register.component.ts
+++ src/app/components/patients/patient-register/patient-register.component.ts
@@ -1,8 +1,8 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {Router, RouterLink} from "@angular/router";
-import {CommonModule} from "@angular/common";
-import {FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
-import {PatientsService} from "../patients.service";
+import { Component, inject, OnInit } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
+import { PatientsService } from "../patients.service";
 
 @Component({
   selector: 'app-patient-register',
@@ -11,15 +11,16 @@ import {PatientsService} from "../patients.service";
   templateUrl: './patient-register.component.html',
   styleUrls: ['./patient-register.component.css']
 })
-export class PatientRegisterComponent implements OnInit{
+export class PatientRegisterComponent implements OnInit {
   plans = [{ id: 1, name: 'Plan A' }, { id: 2, name: 'Plan B' }, { id: 3, name: 'Plan C' }, { id: 4, name: 'Plan D' }];
   patientForm!: FormGroup;
+  isDateTimePickerVisible: number = 0;
 
   constructor(
     private fb: FormBuilder,
     private patientService: PatientsService,
     private router: Router
-    ) {}
+  ) { }
 
   ngOnInit(): void {
     this.patientForm = this.fb.group({
@@ -31,12 +32,22 @@ export class PatientRegisterComponent implements OnInit{
       allergies: ['', [Validators.maxLength(255)]],
       status: [true],
       idPlan: [null, [Validators.required]],
-      tutors: this.fb.array([this.createTutor()])
+      tutors: this.fb.array([this.createTutor()]),
+      sessionDates: this.fb.array([])
+    });
+
+    this.patientForm.get('idPlan')?.valueChanges.subscribe(value => {
+      console.log('Plan seleccionado:', value);
+      this.updateDatePickers(value);
     });
   }
 
   get tutors(): FormArray {
     return this.patientForm.get('tutors') as FormArray;
+  }
+
+  get sessionDates(): FormArray {
+    return this.patientForm.get('sessionDates') as FormArray;
   }
 
   createTutor(): FormGroup {
@@ -55,6 +66,21 @@ export class PatientRegisterComponent implements OnInit{
       this.tutors.removeAt(index);
     }
   }
+
+  addSessionDate(): void {
+    this.sessionDates.push(this.fb.group({
+      sessionDate: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required]
+    }));
+  }
+  removeSessionDate(index: number): void {
+    if (this.sessionDates.length > 1) {
+      this.sessionDates.removeAt(index);
+    }
+  }
+
+
   onSubmit(): void {
     if (this.patientForm.valid) {
       const formValue = this.patientForm.value;
@@ -62,7 +88,7 @@ export class PatientRegisterComponent implements OnInit{
         (response) => {
           this.router.navigate(['/patients']);
         },
-        (error)=>{
+        (error) => {
           console.log('Error al registrar', error);
         }
       )
@@ -75,6 +101,36 @@ export class PatientRegisterComponent implements OnInit{
     const confirmed = window.confirm('¿Estás seguro de que deseas cancelar el registro?');
     if (confirmed) {
       this.router.navigate(['/patients']);
+    }
+  }
+  updateDatePickers(planId: any): void {
+    const numericPlanId = Number(planId);
+    switch (numericPlanId) {
+      case 1:
+        this.isDateTimePickerVisible = 1;
+        break;
+      case 2:
+        this.isDateTimePickerVisible = 2;
+        break;
+      case 3:
+        this.isDateTimePickerVisible = 3;
+        break;
+      case 4:
+        this.isDateTimePickerVisible = 4;
+        break;
+      default:
+        this.isDateTimePickerVisible = 0;
+        break;
+    }
+
+    // Reset the session dates if the number of pickers changes
+    if (this.isDateTimePickerVisible > 0) {
+      while (this.sessionDates.length < this.isDateTimePickerVisible) {
+        this.addSessionDate();
+      }
+      while (this.sessionDates.length > this.isDateTimePickerVisible) {
+        this.removeSessionDate(this.sessionDates.length - 1);
+      }
     }
   }
 }
